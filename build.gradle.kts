857e3c8a09ec39880c29f6cd54842797d2f5a71e
Andrzej Mikulski <fotografandrzejmikulski@gmail.com>
2026-09-24 22:07:36 +0200

    build: align KSP with current Kotlin 2.3 toolchain

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.3.21" apply false
     id("com.google.dagger.hilt.android") version "2.57.1" apply false
-    id("com.google.devtools.ksp") version "2.3.0-1.0.28" apply false
+    id("com.google.devtools.ksp") version "2.3.12" apply false
 }
