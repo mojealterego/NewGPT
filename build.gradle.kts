c7b9cdea3f73916e47f0c15659d72a7d6da8955a
Andrzej Mikulski <fotografandrzejmikulski@gmail.com>
2026-09-24 22:05:17 +0200

    chore: initialize Android project

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("com.android.application") version "8.13.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.21" apply false
+    id("com.google.dagger.hilt.android") version "2.57.1" apply false
+    id("com.google.devtools.ksp") version "2.3.0-1.0.28" apply false
+}
