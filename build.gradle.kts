d313547499dd7f4ef6345d9a84197c156a5dda5e
Andrzej Mikulski <fotografandrzejmikulski@gmail.com>
2026-09-25 12:32:42 +0200

    build: migrate to AGP built-in Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application") version "9.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.3.21" apply false
     id("com.google.dagger.hilt.android") version "2.57.1" apply false
