22e75189899ea2431a8e6ee21132aae4e0ea804a
Andrzej Mikulski <fotografandrzejmikulski@gmail.com>
2026-09-25 14:10:13 +0200

    fix: repair Nexus 3 compilation errors

--- app/src/main/java/com/mojealterego/newgpt/presentation/cognitive/CognitiveCoreScreen.kt
+++ app/src/main/java/com/mojealterego/newgpt/presentation/cognitive/CognitiveCoreScreen.kt
@@ -27,7 +27,9 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier\nimport androidx.compose.ui.semantics.contentDescription\nimport androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.ViewModel
